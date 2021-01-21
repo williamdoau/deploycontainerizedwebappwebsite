3c3cd60dc152cf9df92467a470ff1a44f3541bf7
William Do <wdo@hub24.com.au>
2021-01-22 10:45:14 +1100

    Created project

---
+++ DeployContainerizedWebApp.Website/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DeployContainerizedWebApp.Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
